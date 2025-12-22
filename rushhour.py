30a1251a77e14e777647f0b26a08d15aaa529532
NathanMNZ02 <nathanmonzani17@gmail.com>
2025-12-22 16:17:01 +0100

    Aggiunto timing

--- rushhour.py
+++ rushhour.py
@@ -1,19 +1,23 @@
 import os
 import clingo
 
-benchmarks = os.listdir("asp/benchmarks")
+def start_benchmarks():
+    benchmarks = os.listdir("asp/benchmarks")
 
-for b in benchmarks:
-    print(f"=== ESECUZIONE BENCHMARK: {b} ===")
-    ctl = clingo.Control()
-    ctl.load("asp/rushhour.lp")
-    ctl.load(f"asp/benchmarks/{b}")
-    ctl.ground([("base", [])])
+    for b in benchmarks:
+        print(f"=== ESECUZIONE BENCHMARK: {b} ===")
+        ctl = clingo.Control()
+        ctl.load("asp/rushhour.lp")
+        ctl.load(f"asp/benchmarks/{b}")
+        ctl.ground([("base", [])])
 
-    print("=== FATTI GROUND ===")
-    for atom in ctl.symbolic_atoms:
-        print(atom.symbol)
+        print("=== FATTI GROUND ===")
+        for atom in ctl.symbolic_atoms:
+            print(atom.symbol)
 
-    print("\n=== SOLVING ===")
-    result = ctl.solve(on_model=lambda m: print(f"Model: {m.symbols(atoms=True)}"))
-    print(f"Result: {result}\n\n")
+        print("\n=== SOLVING ===")
+        result = ctl.solve(on_model=lambda m: print(f"Model: {m.symbols(atoms=True)}"))
+        print(f"Result: {result}\n\n")
+        
+if __name__ == "__main__":
+    start_benchmarks()
